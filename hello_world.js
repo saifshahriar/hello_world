829cc6b58c606da2a4371d2d9855e36c57ce26f7
Saif Shahriar <saif.shahriar.pilotbd@gmail.com>
2022-06-11 04:22:30 +0600

    added js

---
+++ hello_world.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+
+/* filename	: hello_world.js
+ * language	: JavaScript
+ *
+ * author	: Saif Shahriar
+ * date		: 11-Jun-2022
+ * repo		: https://github.com/saifshahriar/hello_world
+ *
+ * compile	:
+ * $ node hello_world.js
+ */
+
+let greeting = "Hello World!"
+console.log(greeting)
