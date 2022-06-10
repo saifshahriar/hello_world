8050cc397439fb9ae48dabd2fae42a7549a7a7a8
Saif Shahriar <saif.shahriar.pilotbd@gmail.com>
2022-06-11 04:12:14 +0600

    added rust src code and rust bin

---
+++ hello_world.rs
@@ -0,0 +1,16 @@
+/* filename	: hello_world.rs
+ * language	: Rust
+ *
+ * author	: Saif Shahriar
+ * date		: 11-Jun-2022
+ * repo		: https://github.com/saifshahriar/hello_world
+ *
+ * compile	:
+ * $ rustc hello_world.rs
+ * $ ./hello_world
+ */
+
+fn main() {
+    let greeting = "Hello World!";
+    println!("{}", greeting);
+}
